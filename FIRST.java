178d2b56fd9d4132f599aa1f09c50fe12e69a55e
AKSHAYA E M <emakshaya11@gmail.com>
2026-03-02 01:24:43 +0530

    Add files via upload

---
+++ FIRST.java
@@ -0,0 +1,23 @@
+package Pattern;
+/*
+ 
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+
+ */
+
+public class FIRST {
+	public static void main(String[] args) {
+		int n=5;
+		for(int row=1;row<=n;row++) {
+			for(int col=1;col<=n;col++) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+
+}
